153058f3763582484e2cc94768f9609ddd954381
yaroslavchik <yaroslavsalata@gmail.com>
2017-05-10 21:07:12 +0300

    debugging v 1.01

--- main.c
+++ main.c
@@ -26,13 +26,13 @@ void		gameplay(t_info *g)
 	sum = (pos == 1) ?\
 		  ADD(g_i, g_j) : ADD(g->map->x - g_i, g->map->y - g_j);
 
-	ft_putstr_fd("\nGAMPLAY START !!!\n",g_fd1);
+	ft_putstr_fd("x: ",g_fd1);
 	ft_putnbr_fd(g_i, g_fd1);
-	ft_putchar_fd(' ', g_fd1);
+	ft_putstr_fd(" y: ",g_fd1);
 	ft_putnbr_fd(g_j, g_fd1);
-	ft_putchar_fd(' ', g_fd1);
+	ft_putstr_fd(" len: ",g_fd1);
 	ft_putnbr_fd(sum, g_fd1);
-	ft_putstr_fd("\nGAMPLAY END !!!\n",g_fd1);
+	ft_putstr_fd("\n",g_fd1);
 	if (g->len < sum)
 		return ;
 	g->x = g_i;
@@ -45,18 +45,18 @@ int		analyze(t_info *g)
 	int a;
 
 	g_i = -1;
-
+	ft_putstr_fd("\nStart analyzing ... \n",g_fd1);
 	while(++g_i <= g->hight)
 	{
 		g_j = -1;
-		while (++g_j < g->wid)
+		while (++g_j <= g->wid)
 		{
 			a = 1;
 			g_n = -1;
 			 while (++g_n < g->fig->x)
 			 {
 				 g_m = -1;
-				 while (++g_m <= g->fig->y)
+				 while (++g_m < g->fig->y)
 					 if (g->fig->field[g_n][g_m] == '*' && \
 					 	g->map->field[g_i + g_n][g_j + g_m] != '.')
 						 a = (g->map->field[g_i + g_n][g_j + g_m] !=\
@@ -75,11 +75,12 @@ int 	readform(t_cord *fig)
 	int		i;
 
 	i = -1;
-	ft_putstr_fd("READFORM()",g_fd1);
+	ft_putstr_fd("readform()",g_fd1);
 	get_next_line(g_fd, &fig->trash);
 	fig->x = ft_atoi(fig->trash + 6);
 	fig->y = ft_atoi(fig->trash + ((fig->x < 10) ? 8 : 9));
-	fig->field = (char **)malloc(sizeof(char *) * fig->x);
+	ft_strclr(fig->trash);
+	fig->field = (char **)malloc(sizeof(char *) * fig->x + 1);
 	while (++i < fig->x)
 	{
 		fig->field[i] = (char *) malloc(sizeof(char) * fig->y + 1);
@@ -91,7 +92,7 @@ int 	readform(t_cord *fig)
 	i = -1;
 //	while (++i < fig->x)
 //		printf("%s\n", fig->field[i]);
-	//free(fig->field);
+//	free(fig->field);
 	return 1;
 }
 
@@ -100,11 +101,11 @@ int		readmap(t_cord *map, char *s)
 	int			i;
 
 	i = -1;
-	ft_putstr_fd("READMAP()\n",g_fd1);
+	ft_putstr_fd("readmap()\n",g_fd1);
 	map->x = ft_atoi(s + 8);
 	map->y = ft_atoi(s + ((map->x < 100) ? 11 : 12));
 	get_next_line(g_fd, &map->trash);
-	map->field = (char **)malloc(sizeof(char *) * map->x);
+	map->field = (char **)malloc(sizeof(char *) * map->x + 1);
 	while (++i < map->x)
 	{
 		map->field[i] = (char *) malloc(sizeof(char) * map->y + 1);
@@ -116,7 +117,7 @@ int		readmap(t_cord *map, char *s)
 	i = -1;
 //	while (++i < map->x)
 //		printf("%s\n", map->field[i]);
-	//free(map->field);
+//	free(map->field);
 	return 1;
 }
 
@@ -133,7 +134,7 @@ int i = 0;
 	pos = 0;
 	while (get_next_line(g_fd, &s))
 	{
-		ft_putstr_fd("STEP №",g_fd1);
+		ft_putstr_fd("\n STEP №",g_fd1);
 		ft_putnbr_fd(i++, g_fd1);
 		ft_putchar_fd('\n', g_fd1);
 		t_i.len = 1000;
@@ -150,14 +151,14 @@ int i = 0;
 		write(1, " ", 1);
 		ft_putnbr(t_i.y);
 		write(1, "\n", 1);
-		ft_putstr_fd("t_x to write:", g_fd1);
+		ft_putstr_fd("t_x to write: ", g_fd1);
 		ft_putnbr_fd(t_i.x, g_fd1);
 		ft_putchar_fd('\n', g_fd1);
-		ft_putstr_fd("t_y to write:", g_fd1);
+		ft_putstr_fd("t_y to write: ", g_fd1);
 		ft_putnbr_fd(t_i.y, g_fd1);
 		ft_putchar_fd('\n', g_fd1);
-		//free(t_i.map);
-		//free(t_i.fig);
+		free(t_i.map);
+		free(t_i.fig);
 		if (t_i.len == 1000) {
 			ft_putstr("0 0\n");
 			return 0;
