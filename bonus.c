e7d7551e968bcda81dea9b3c59dc1c8e62f839af
yaroslavchik <yaroslavsalata@gmail.com>
2017-05-24 21:29:55 +0300

    final v0.1

--- bonus.c
+++ bonus.c
@@ -14,16 +14,12 @@
 #include <fcntl.h>
 #include "libft/libft.h"
 #include "libft/ft_printf/ft_printf.h"
-
-
-int 	g_rp1;
-int		g_rp2;
+#include <wchar.h>
+int 	g_c;
 int		g_i;
 int 	g_y;
-wchar_t	g_p1;
-wchar_t	g_p2;
-int     res1;
-int     res2;
+int     g_retx;
+int     g_rety;
 
 void	message(int i)
 {
@@ -46,23 +42,16 @@ void	message(int i)
 
 int			valid(char *s)
 {
-	int		ret;
-
-	ret = 0;
-	if (ft_strcmp(s, "WW2") == 0 && (ret = 1))
-		g_p1 = 1;
-	else if (ft_strcmp(s, "humans") == 0 && (ret = 2))
-		g_p1 = 1;
-	else if (ft_strcmp(s, "flowers") == 0 && (ret = 3))
-		g_p1 = 3;
+	if (ft_strcmp(s, "WW2") == 0)
+		g_c = 1;
+	else if (ft_strcmp(s, "1969") == 0)
+		g_c = 2;
+	else if (ft_strcmp(s, "UAH") == 0)
+		g_c = 3;
+	else if (ft_strcmp(s, "alpha") == 0)
+		g_c = 4;
 	else
 		return (0);
-	if (ret == 1)
-		g_p2 = 5350;
-	else if (ret == 2)
-		g_p2 = 2;
-	else
-		g_p2 = 3;
 	return (1);
 }
 
@@ -72,7 +61,6 @@ int boarddrow(char *s, int y)
 	int n;
 	int flag;
 
-	x = 0;
 	flag = 1;
 	if (!y)
 	{
@@ -80,8 +68,11 @@ int boarddrow(char *s, int y)
 			y = ft_atoi(s + ((x != 100) ? 11 : 12));
 		else if (s[1] == 'i' ? x = ft_atoi(s + 6) : 0)
 			y = ft_atoi(s + ((x < 10) ? 8 : 9));
+		else
+			x = 0;
 		y *= 2;
-		//return 0;
+		s[1] == 'l' ? ft_printf("\nMAP [%d x %d]", x, y) : 0;
+		s[1] == 'i' ? ft_printf("\nFIELD [%d x %d]", x, y) : 0;
 	}
 	n = y;
 	write(1, "\n", 1);
@@ -96,19 +87,23 @@ int boarddrow(char *s, int y)
 
 int draw(char *s)
 {
+	//wchar_t symb[5] = {☭☺₴A࿘☻$Z}
+	wint_t *symb[9] = {L"☭", L"☭", L"☺", L"₴", L"A", L"࿘", \
+		L"☻", L"$", L"Z"};
+	s[0] == '<' ? ft_printf("FOR PLAYER: |") : 0;
 	while (s[++g_i])
 		if (s[g_i] == 'x' || s[g_i] == 'X')
-			ft_printf(RED" %S |"RC, L"☭");
+			ft_printf(COL_RED" %S |"COL_EOC, symb[g_c]);
 		else if ((s[g_i] == 'o' && s[g_i - 1] != 'g' && \
 					s[g_i - 1] != 'i') || s[g_i] == 'O')
-			ft_printf(BLU" %S |"RC, L"࿘");
+			ft_printf(COL_MAGENTA" %S |"COL_EOC, \
+			symb[g_c + 4]);
 		else if (s[g_i] == '.')
 			s[g_i] == '.' ? ft_printf("   |") : 0;
 		else if (s[g_i] == '*' ? \
-			ft_printf(WHT" # "RC) : 0)
+			ft_printf(COL_WHITE" # "COL_EOC) : 0)
 			write(1, "|", 1);
-        else
-            write (1, &s[g_i], 1);
+		s[0] == '<' ? write(1, "\n", 1) : 0;
 		//else
 		//	write(1, &s[g_i], 1);
 		return (1);
@@ -120,9 +115,9 @@ int main (int ac, char **av)
 	char	*s;
 	int		type;
 
-    a = 1;
+    a = 2;
 	if (((ac != 2) || !valid(av[1]))? \
-		ft_printf("usage: ./bonus [ww2 || ras || gender]") : 0)
+		ft_printf("usage: ./bonus [ww2 || 1969 || gender]") : 0)
 		return (0);
     message(1);
 	while (get_next_line(0, &s))
@@ -131,12 +126,20 @@ int main (int ac, char **av)
 		if (s[0] == ' ')
 			continue ;
 		s[0] == 'P' ? g_y = boarddrow(s, 0) : 0;
+		if (s[0] == '=' ? a--: 0)
+		{
+			while (!ft_isdigit(*s))
+				s++;
+			a == 1? g_retx = ft_atoi(s) : 0;
+			a == 0 ? g_rety = ft_atoi(s) : 0;
+			continue ;
+		}
 		if ((ft_isdigit(*s) ? s += 4 : 0) || *s == '.' || *s == '*')
 			write(1, "|", 1);
 		draw(s);
-		s[0] == 'P' ? 0 : boarddrow(s, g_y);
-        if (s[0] == '=' ? a--: 0)
-            if (a == 0 ? :  )
+		s[0] == 'P' || s[0] == '<' ? 0 : boarddrow(s, g_y);
+      //  if (s[0] == '=' ? a--: 0)
+        //    if (a == 0 ? :  g_p1)
         //s[0] == '=' ?
 	}
     message(2);
